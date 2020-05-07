907264ab63085660bcc43c9bda8d6227fdc95b19
sxc <qiuyequ@icloud.com>
2020-05-07 16:29:45 +0800

    start programming MapView

--- Quiz/Quiz/QuizViewController.swift
+++ Quiz/Quiz/QuizViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class QuizViewController: UIViewController {
 
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
