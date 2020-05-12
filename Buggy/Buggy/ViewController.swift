95ad3551a3b8d4188af76ec14524fe66a0f538a3
sxc <qiuyequ@icloud.com>
2020-05-12 13:07:01 +0800

    Debug

--- Buggy/Buggy/ViewController.swift
+++ Buggy/Buggy/ViewController.swift
@@ -16,5 +16,23 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func buttonTapped(_ sender: UIButton){
+        print("Method: \(#function) in file: \(#file) line: \(#line) called.")
+        
+        badMethod()
+    }
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        
+        for i in 0..<10 {
+            array.insert(i, at: i)
+        }
+        
+        // Go one step too far empty the array (notice the range change):
+        for _ in 0..<10 {
+            array.removeObject(at: 0)
+        }
+    }
 }
 
