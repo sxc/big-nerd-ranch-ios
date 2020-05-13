583effff5067a4d90e2bca05d741c02f55811d16
sxc <qiuyequ@icloud.com>
2020-05-13 09:45:37 +0800

    Updating the interface for editing mode

--- LootLogger/LootLogger/ItemsViewController.swift
+++ LootLogger/LootLogger/ItemsViewController.swift
@@ -17,6 +17,20 @@ class ItemsViewController: UITableViewController {
     }
     
     @IBAction func toggleEditingMode(_ sender: UIButton) {
+        // If you are currently in editing mode
+        if isEditing {
+            // Change text of button to inform user of state
+            sender.setTitle("Edit", for: .normal)
+            
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        } else {
+            // Change text of button to infor user of state
+            sender.setTitle("Done", for: .normal)
+            
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
         
     }
     
