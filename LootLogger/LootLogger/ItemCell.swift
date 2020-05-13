8a297c38b1274135a0dcc7ec89822f6446cbb542
sxc <qiuyequ@icloud.com>
2020-05-13 15:21:59 +0800

    automatic row height

---
+++ LootLogger/LootLogger/ItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCell.swift
+//  LootLogger
+//
+//  Created by Xiaochun Shen on 2020/5/13.
+//  Copyright © 2020 SXC. All rights reserved.
+//
+
+import UIKit
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
