5a316c942b14888bc033fd87d9d65e9d45de7d07
sxc <qiuyequ@icloud.com>
2020-05-17 19:21:54 +0800

    add new project mood and color set

---
+++ Mandala/Mandala/Mood.swift
@@ -0,0 +1,15 @@
+//
+//  Mood.swift
+//  Mandala
+//
+//  Created by Xiaochun Shen on 2020/5/17.
+//  Copyright © 2020 SXC. All rights reserved.
+//
+
+import UIKit
+
+struct Mood {
+    var name: String
+    var imgae: UIImage
+    var color: UIColor
+}
---
+++ Mandala/Mandala/MoodEntry.swift
@@ -0,0 +1,14 @@
+//
+//  MoodEntry.swift
+//  Mandala
+//
+//  Created by Xiaochun Shen on 2020/5/17.
+//  Copyright © 2020 SXC. All rights reserved.
+//
+
+import Foundation
+
+struct MoodEntry {
+    var mood: Mood
+    var timestamp: Date
+}
