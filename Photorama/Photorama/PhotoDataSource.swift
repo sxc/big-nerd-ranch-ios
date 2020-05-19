1ca38cc2a17693a553fb76b3d74d587bb7bbbc54
sxc <qiuyequ@icloud.com>
2020-05-19 14:45:19 +0800

    add Collection View

---
+++ Photorama/Photorama/PhotoDataSource.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Xiaochun Shen on 2020/5/19.
+//  Copyright © 2020 SXC. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    
+    var photos = [Photo]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let identifier = "PhotoCollectionViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        return cell 
+    }
+}
--- Photorama/Photorama/PhotosViewController.swift
+++ Photorama/Photorama/PhotosViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class PhotosViewController: UIViewController {
     
-    @IBOutlet private var imageView: UIImageView!
+//    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet var collectionView: UICollectionView!
+    
     var store: PhotoStore!
-
+    let photoDataSource = PhotoDataSource()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = photoDataSource
         
         store.fetchInterestingPhotos {
             (photosResult) in
@@ -22,27 +27,38 @@ class PhotosViewController: UIViewController {
             switch photosResult {
             case let .success(photos):
                 print("Successfully found \(photos.count) photos.")
-                if let firstPhoto = photos.first {
-                    self.updateImageView(for: firstPhoto)
-                }
+                
+                //                if let firstPhoto = photos.first {
+                //                    self.updateImageView(for: firstPhoto)
+                //                }
+                
+                self.photoDataSource.photos = photos
+                
             case let .failure(error):
                 print("Error fetching interesting photos: \(error)")
+                
+                self.photoDataSource.photos.removeAll()
             }
+            
+            self.collectionView.reloadSections(IndexSet(integer: 0))
         }
+        
+        
+        
     }
 
-    func updateImageView(for photo: Photo) {
-        store.fetchImage(for: photo) {
-            (imageResult) in
-            
-            switch imageResult {
-            case let .success(image):
-                self.imageView.image = image
-            case let .failure(error):
-                print("Error downloading image: \(error)")
-            }
-        }
-    }
+//    func updateImageView(for photo: Photo) {
+//        store.fetchImage(for: photo) {
+//            (imageResult) in
+//
+//            switch imageResult {
+//            case let .success(image):
+//                self.imageView.image = image
+//            case let .failure(error):
+//                print("Error downloading image: \(error)")
+//            }
+//        }
+//    }
 
 }
 
