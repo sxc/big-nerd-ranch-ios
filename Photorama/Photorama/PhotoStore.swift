bd75e4ca2541fa1449ae1050016fa8d327c50dc1
sxc <qiuyequ@icloud.com>
2020-05-20 09:11:05 +0800

    store phto to image

--- Photorama/Photorama/PhotoStore.swift
+++ Photorama/Photorama/PhotoStore.swift
@@ -15,6 +15,8 @@ enum PhotoError: Error {
 
 class PhotoStore {
     
+    let imageStore = ImageStore()
+    
     
     private let session: URLSession = {
             let config = URLSessionConfiguration.default
@@ -58,6 +60,16 @@ class PhotoStore {
     
     func fetchImage(for photo: Photo, completion: @escaping
         (Result<UIImage, Error>) -> Void) {
+        
+        let photoKey = photo.photoID
+        if let image = imageStore.image(forKey: photoKey) {
+            OperationQueue.main.addOperation {
+                completion(.success(image))
+            }
+            return
+        }
+        
+        
         guard let photoURL = photo.remoteURL
         else  {
             completion(.failure(PhotoError.missingImageURL))
@@ -66,9 +78,14 @@ class PhotoStore {
         }
         let request = URLRequest(url: photoURL)
         let task = session.dataTask(with: request) {
-            (data, response, error) in
+            (data, response, error) -> Void in
             
             let result = self.processImageRequest(data: data, error: error)
+            
+            if case let .success(image) = result {
+                self.imageStore.setImage(image, forKey: photoKey)
+            }
+            
             OperationQueue.main.addOperation {
                 completion(result)
             }
