d1088e30d889d35212d7d8669897acd5c3581bd9
sxc <qiuyequ@icloud.com>
2020-05-26 13:09:36 +0800

    add new project FlickrDemo

---
+++ FlickrDemo/FlickrDemo/PhotosViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FlickrDemo
+//
+//  Created by Xiaochun Shen on 2020/5/26.
+//  Copyright © 2020 SXC. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    @IBOutlet private var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
