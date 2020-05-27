5c91c734a3f123dc053ca2ebb2203626316518a1
sxc <qiuyequ@icloud.com>
2020-05-27 11:24:05 +0800

    Request JSON

---
+++ FlickrDemo/FlickrDemo/FlickrAPI.swift
@@ -0,0 +1,58 @@
+//
+//  FlickrAPI.swift
+//  FlickrDemo
+//
+//  Created by Xiaochun Shen on 2020/5/26.
+//  Copyright © 2020 SXC. All rights reserved.
+//
+
+import Foundation
+
+
+enum EndPoint: String {
+    case interestingPhotos = "flickr.interestingness.getList"
+}
+
+struct FlickrAPI {
+    
+    private static let baseURLString = "https://api.flickr.com/services/rest"
+    private static let apiKey = "a6d819499131071f158fd740860a5a88"
+    
+    private static func flickrURL(endPoint: EndPoint, parameters: [String: String]?) -> URL {
+        
+//        return URL(string: "")!
+        
+        var components = URLComponents(string: baseURLString)!
+        var queryItems = [URLQueryItem]()
+        
+        let baseParams = [
+            "method": endPoint.rawValue,
+            "format": "json",
+            "nojsoncallback": "1",
+            "api_key": apiKey
+        ]
+        
+        for (key, value) in baseParams {
+            let item = URLQueryItem(name: key, value: value)
+            queryItems.append(item)
+        }
+        
+        if let additionalParams = parameters {
+            for (key, value) in additionalParams {
+                let item = URLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+        }
+        components.queryItems = queryItems
+        
+        if let componetsURL = components.url {
+            print("URL = \(componetsURL)")
+        }
+        return components.url!
+        
+    }
+    
+    static var interestingPhotosURL: URL {
+        return flickrURL(endPoint: .interestingPhotos, parameters: ["extras": "url_z, date_taken"])
+    }
+}
